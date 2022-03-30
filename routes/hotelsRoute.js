cbec958b0f13080d8447bd6ac43c2f222218734c
clement-tour <86768151+clement-tour@users.noreply.github.com>
2022-03-30 17:42:57 +0400

    Modify hotel added

--- routes/hotelsRoute.js
+++ routes/hotelsRoute.js
@@ -5,7 +5,6 @@ const Hotel = require("../models/hotel");
 
 router.get("/getallhotels", async (req, res) => {
   try {
-    console.dir(req.params);
     const hotels = await Hotel.find({});
     res.send(hotels);
   } catch (error) {
@@ -26,6 +25,24 @@ router.post("/addhotel", async (req, res) => {
   try {
     await newhotel.save();
     res.send("La nouvelle chambre a bien été ajouté");
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.put("/addhotel", async (req, res) => {
+  const { hotelid, name, city, address, description } = req.body;
+  console.log(req.body);
+  try {
+    const hotelTemp = await Hotel.findOne({ _id: hotelid });
+    console.log(hotelTemp);
+    hotelTemp.name = name;
+    hotelTemp.city = city;
+    hotelTemp.address = address;
+    hotelTemp.description = description;
+
+    await hotelTemp.save();
+    res.send("L'hôtel a bien été modifié'");
   } catch (error) {
     return res.status(400).json({ error });
   }
